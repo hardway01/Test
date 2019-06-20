0848f6db1c84f1f2bc443f149d6cc388950719f5
Andries Fokkema <andries_fokkema@epam.com>
2019-06-20 16:47:42 +0200

    Update empty_body.py
    Updated with put request

--- empty_body.py
+++ empty_body.py
@@ -19,7 +19,17 @@ for comment in all_comments:
         comment["body"] = ""
 #        print(ids_dict.items())
 
-response = requests.post(base_url + resource) #https://jsonplaceholder.typicode.com/comments/
-print(response.status_code)
-pprint(response.headers, indent=2)
-pprint(response.json())
+#response = requests.post(base_url + resource, ) #https://jsonplaceholder.typicode.com/comments/
+#print(response.status_code)
+#pprint(response.headers)
+#pprint(response.json())
+
+for comment in all_comments:
+    if comment['id'] == 6:
+        comment["body"] = "New Body"
+        print(ids_dict.items())
+
+reponse = requests.put('https://jsonplaceholder.typicode.com/comments/6', ids_dict)
+print(reponse.status_code)
+
+
