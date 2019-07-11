bd962e5737b888684596abaf39997b16420f35d9
Chingiz_Toimbetov <ctoimbetov.contractor@libertyglobal.com>
2019-07-11 10:40:38 +0200

    selenium comments

--- Selenium.py
+++ Selenium.py
@@ -1,33 +1,27 @@
-import time
 from selenium import webdriver
+
+
 driver = webdriver.Chrome()
+driver.implicitly_wait(30)
 driver.get("https://www.nu.nl")
-time.sleep(2)
 
 button = driver.find_element_by_id("sanoma-consent-accept-button")
 button.click()
-time.sleep(2)
 
-article = driver.find_element_by_link_text("Merkel ontkent gezondheidsproblemen na derde trilaanval")
+article = driver.find_element_by_link_text("Merkel ontkent gezondheidsproblemen na derde trilaanval")# this text is not exist right now, so search by text is not relayable(if text change)
 article.click()
-time.sleep(2)
 
-driver.quit()
+# you can reuse previosly created browser instance and directly call 'driver.get("YOUR URL")'
 
-driver = webdriver.Chrome()
 driver.get("https://www.bbc.com/")
-time.sleep(2)
 
 search_input = driver.find_element_by_id("orb-search-q")
 search_input.send_keys("Trump")
-time.sleep(2)
 search_button = driver.find_element_by_id("orb-search-button")
 search_button.click()
-time.sleep(2)
 
 #article = driver.find_element_by_link_text("Panorama: Trump: Is the President a Sex Pest?")
-article = driver.find_element_by_xpath("/html/body/div[6]/section[2]/ol/li[3]/article/div/h1/a")
+article = driver.find_element_by_xpath("/html/body/div[6]/section[2]/ol/li[3]/article/div/h1/a") # it would be better to avoid using "GENERATED xpaths" because they are rely on static page structure
 article.click()
-time.sleep(5)
 
 driver.quit()
